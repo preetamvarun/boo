454bf034cb8e583da6bfa98a81bafedc114b2ec2
preetam varun <49122112+preetamvarun@users.noreply.github.com>
2024-04-15 10:42:56 +0100

    Update BookRepository.java

--- backend/src/main/java/com/myproject/example/book/JpaRepo/BookRepository.java
+++ backend/src/main/java/com/myproject/example/book/JpaRepo/BookRepository.java
@@ -3,6 +3,9 @@ package com.myproject.example.book.JpaRepo;
 import com.myproject.example.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+/* The book repository interface is inheriting all the methods from the parent interface which is JpaRepository. The JpaRepository provides method for CRUD operations
+when working with database entities. Book is the entity type that is going to be managed and Integer is the type of the primary key that his particular entity is having*/
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
 }
@@ -10,4 +13,5 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
 /*
 Hibernate is a framework used for persisting java objects in the database.
 Advantage with hibernate is it handles all the low-level sql. entityManager.persist(obj);
+
 */
