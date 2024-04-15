955dc9490200978127d1b95b4c3379bce97e55bf
preetam varun <49122112+preetamvarun@users.noreply.github.com>
2024-04-15 10:25:26 +0100

    Update BookRepository.java

--- backend/src/main/java/com/myproject/example/book/JpaRepo/BookRepository.java
+++ backend/src/main/java/com/myproject/example/book/JpaRepo/BookRepository.java
@@ -6,3 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
 }
+
+/*
+Hibernate is a framework used for persisting java objects in the database.
+Advantage with hibernate is it handles all the low-level sql. entityManager.persist(obj);
+*/
