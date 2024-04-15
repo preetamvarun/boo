c06e747799a1b9a8a9b0dd16802659f2ccdd7b7b
preetam varun <49122112+preetamvarun@users.noreply.github.com>
2024-04-15 09:55:42 +0100

    Update BookApplication.java

--- backend/src/main/java/com/myproject/example/book/BookApplication.java
+++ backend/src/main/java/com/myproject/example/book/BookApplication.java
@@ -3,6 +3,15 @@ package com.myproject.example.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+@SpringBootApplicaiton = @Configuration + @EnableAutoConfiguration + @ComponentScan
+
+@Configuration is a class level annotation that indicates that this class contains one or more bean methods that returns a bean which is managed by the container.
+@EnableAutoConfiguration figurest out what are all the necessary configurations that this particular app needs to start and does that.
+when we specify @ComponentScan spring boot scans for all those classes that are annotated with @Component, @Service, @Repository, @Controller, @RestController etc.,
+It will creates beans which are then managed by the spring IOC container.
+*/
+
 @SpringBootApplication
 public class BookApplication {
 
