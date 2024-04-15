ca266e688b37afec7e4aad5cedefdd0a203a1b85
preetam varun <49122112+preetamvarun@users.noreply.github.com>
2024-04-15 10:31:12 +0100

    Update Book.java

--- backend/src/main/java/com/myproject/example/book/entity/Book.java
+++ backend/src/main/java/com/myproject/example/book/entity/Book.java
@@ -1,5 +1,17 @@
 package com.myproject.example.book.entity;
 
+/*
+  Hibernate provides ORM (object to relational mappinp). In orm, the developers define the mapping between java table and database table.
+  JPA stands for jakarta persistence Api.
+  JPA is not a framework it is a specification that defines a set of interfaces and requirements. There are some frameworks out there that implements these interfaces
+  from JPAspec and provides their implementation. Hibernate is used by default in spring boot.
+  For example, if we want to save a java object into a database, entityManager.persist(obj); entityManager is a JPA helper object. it handles low level sql.
+  for example, if we want to find a student from the database using an id which is a primary  key we write
+  Student s = entityManager.find(Student.class, id);
+*/
+
+
+
 import jakarta.persistence.*;
 @Entity
 @Table(name = "books")
@@ -21,6 +33,7 @@ public class Book {
     public Book(){
 
     }
+    
     public Book(int id, String title, String authorName, int pageCount) {
         Id = id;
         Title = title;
